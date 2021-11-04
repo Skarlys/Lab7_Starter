1067163d8709a109a526eaa98d0ffbf84ebd0752
Danica <danica.xiong@yahoo.com>
2021-11-04 11:30:51 -0700

    why does it work for safari and not chrome

--- sw.js
+++ sw.js
@@ -52,16 +52,36 @@ self.addEventListener('fetch', function (event) {
    */
 
   //https://developers.google.com/web/fundamentals/primers/service-workers#cache_and_return_requests
-   event.respondWith(
+  event.respondWith(
     caches.match(event.request)
       .then(function(response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
-  );
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 
 });
